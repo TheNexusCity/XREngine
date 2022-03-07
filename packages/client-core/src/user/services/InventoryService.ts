940d6b1a4de56a18902eb0389aa8d3325934a2e6
dreamworker <metadev26@gmail.com>
2022-03-07 15:43:32 -0600

    NFT modueling

--- packages/client-core/src/user/services/InventoryService.ts
+++ packages/client-core/src/user/services/InventoryService.ts
@@ -4,18 +4,8 @@ import { useDispatch, store } from '../../store'
 import { client } from '../../feathers'
 import { createState, DevTools, useState, none, Downgraded } from '@speigg/hookstate'
 import { RelationshipSeed } from '@xrengine/common/src/interfaces/Relationship'
-import { ethers } from 'ethers'
-import Moralis from 'moralis'
-import axios from 'axios'
-import { Principal } from '@dfinity/principal'
-import { NFTIDL } from '../../util/nft.did'
-
-/**
- * Moralis SDK config - ERC721
- */
-const serverUrl = 'https://s0vkti4ccvl3.usemoralis.com:2053/server' // Moralis Server Url here
-const appId = 'DFDqRRGNHvPpOFmVqNSEkNFtSYUkQfLpY4UOZyw7' // Moralis Server App ID here
-Moralis.start({ serverUrl, appId })
+import { getMyDIP721Tokens } from '../functions/DIP721'
+import { getMyERC721Tokens } from '../functions/ERC721'
 
 //State
 const state = createState({
@@ -58,50 +48,6 @@ store.receptors.push((action: InventoryActionType): void => {
 export const accessInventoryState = () => state
 export const useInventoryState = () => useState(state) as any as typeof state as unknown as typeof state
 
-// Blockchain ERC721 NFT metadata
-const apiKey = '3KR3QB1T46GYWMING2CFTN6FPZEBT7SIRV'
-const corsUrl = 'https://arcane-eyrie-83731.herokuapp.com/'
-const InventoryNFTService = {
-  contracts: {},
-  getCorsUrl: (url) => {
-    return corsUrl + url
-  },
-
-  getPinataUrl: (url) => {
-    if (!url) return ''
-    if (url.includes('ipfs://')) url = url.replace('ipfs://', 'https://ipfs.io/ipfs/')
-    if (url.includes('gateway.pinata.cloud')) url = url.replace('gateway.pinata.cloud', 'ipfs.io')
-    return url
-  },
-
-  getContract: async (address) => {
-    if (!InventoryNFTService.contracts[address]) {
-      let response = await axios.get(
-        `https://api.etherscan.io/api?module=contract&action=getabi&address=${address}&apikey=${apiKey}`
-      )
-      if (response.status !== 200) return { status: 0 }
-      const abi = response.data.result
-      console.warn('abi: ', abi)
-      const provider = new ethers.providers.EtherscanProvider('homestead', apiKey)
-      const contract = new ethers.Contract(address, abi, provider)
-      InventoryNFTService.contracts[address] = contract
-    }
-    return InventoryNFTService.contracts[address]
-  },
-  getTokenMetadata: async (address, tokenId) => {
-    try {
-      const contract = InventoryNFTService.getContract(address)
-      let response = await (contract as any).functions.tokenURI(tokenId)
-      if (!response.length) return { status: 0 }
-      const tokenURI = response[0]
-      response = await axios.get(InventoryNFTService.getCorsUrl(InventoryNFTService.getPinataUrl(tokenURI)))
-      return { status: response.status, data: response.data }
-    } catch {
-      return { status: 0 }
-    }
-  }
-}
-
 //Service
 export const InventoryService = {
   handleTransfer: async (ids, itemid, inventoryid) => {
@@ -134,26 +80,9 @@ export const InventoryService = {
       /**
        * ERC721 NFT Sync
        */
-
-      // A Web3Provider wraps a standard Web3 provider, which is
-      // what MetaMask injects as window.ethereum into each page
-      // const provider = new ethers.providers.Web3Provider((window as any).ethereum)
-
-      // // MetaMask requires requesting permission to connect users accounts
-      // await provider.send('eth_requestAccounts', [])
-
-      // The MetaMask plugin also allows signing transactions to
-      // send ether and pay to change state within the blockchain.
-      // For this, you need the account signer...
-      // const signer = provider.getSigner()
-
-      // const walletAddress = await signer.getAddress()
-
-      // const options = { chain: 'rinkeby', address: walletAddress } as any
-      // const myNftData = await Moralis.Web3API.account.getNFTs(options)
-
-      // myNftData.result?.forEach(async (item) => {
-      //   if (item.metadata) {
+      // const myNftData = await getMyERC721Tokens()
+      // myNftData.forEach((item) => {
+      //   if( item.metadata ) {
       //     const meta = JSON.parse(item.metadata)
       //     inventory_items.push({
       //       ...invenItem,
@@ -168,35 +97,14 @@ export const InventoryService = {
       /**
        * DIP721 NFT Sync
        */
-      const nftCanisterId = 'vlhm2-4iaaa-aaaam-qaatq-cai'
+      const myNFTs = await getMyDIP721Tokens()
 
-      const hasAllowed = await (window as any).ic?.plug?.requestConnect({
-        whitelist: [nftCanisterId] // whitelisting canister ID's for plug
-      })
-      if (!hasAllowed) {
-        console.error('allow the canisters')
-      }
-
-      const wallet = await (window as any).ic?.plug?.getPrincipal()
-      const walletAddress = wallet.toText()
-
-      const nftActor = await (window as any).ic?.plug?.createActor({
-        canisterId: nftCanisterId,
-        interfaceFactory: NFTIDL.factory
-      })
-
-      const name = await nftActor?.nameDip721()
-      const symbol = await nftActor?.symbolDip721()
-
-      // My NFTS
-      const myNFTs = await nftActor?.getMetadataForUserDip721(Principal.fromText(walletAddress))
-
-      myNFTs.forEach((item) => {
+      ;(myNFTs as any).forEach((item) => {
         inventory_items.push({
           ...invenItem,
           user_inventory: { quantity: 1 },
           slot: inventory_items.length,
-          name: name + item.token_id,
+          name: item.token_id,
           url: item.metadata_desc[0].key_val_data[4].val.TextContent
         })
       })
